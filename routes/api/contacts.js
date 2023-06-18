e26bea90586d4a7cf21b120635503be07973b7e7
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2023-06-18 22:43:08 +0300

    routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,44 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const ctrl = require("../../controllers/contacts");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const { validateBody } = require("../../middlewares");
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const schemas = require("../../schemas/contacts");
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+const router = express.Router();
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", ctrl.getAll);
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:id", ctrl.getById);
 
-module.exports = router
+router.get("/", ctrl.getAll);
+
+router.post("/", validateBody(schemas.addSchema), ctrl.add);
+
+router.delete("/:id", ctrl.deleteById);
+
+router.put("/:id", validateBody(schemas.addSchema), ctrl.updateById);
+
+module.exports = router;
+
+// router.get("/", async (req, res, next) => {
+//   const result = await contacts.listContacts();
+//   res.json(result);
+// });
+
+// router.get("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+// router.post("/", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+// router.delete("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
+
+// router.put("/:contactId", async (req, res, next) => {
+//   res.json({ message: "template message" });
+// });
