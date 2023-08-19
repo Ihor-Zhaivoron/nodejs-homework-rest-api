3a5c49a4487a105024c9bab5e80ea16c47b64829
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2023-08-19 20:39:11 +0200

    check error

--- server.js
+++ server.js
@@ -1,24 +1,19 @@
 const mongoose = require("mongoose");
-const { DB_HOST, PORT = 3000 } = process.env;
-mongoose.set("strictQuery", true);
-const DB_HOST="mongodb+srv://Ihorok:Makarosha2208@@cluster0.dbmxdyh.mongodb.net/db_contacts?retryWrites=true&w=majority"
-
 const app = require('./app')
+require("dotenv").config();
 
+const { DB_HOST, PORT = 3000 } = process.env;
 
-
+mongoose.set("strictQuery", true);
 mongoose
   .connect(DB_HOST)
   .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: ${PORT}`);
+    });    
     console.log("Database connection successful");
-    app.listen(3000);
   })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
-
-// ///
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
