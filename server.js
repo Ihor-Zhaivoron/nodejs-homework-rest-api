1a58afdff19e0b0b615a407358e1f35d67f8e510
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2023-08-19 17:17:50 +0200

    reg on render.com

--- server.js
+++ server.js
@@ -1,5 +1,24 @@
+const mongoose = require("mongoose");
+const { DB_HOST, PORT = 3000 } = process.env;
+mongoose.set("strictQuery", true);
+const DB_HOST="mongodb+srv://Ihorok:Makarosha2208@@cluster0.dbmxdyh.mongodb.net/db_contacts?retryWrites=true&w=majority"
+
 const app = require('./app')
 
+
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    console.log("Database connection successful");
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// ///
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
 })
