bb5be88e3ab84d512c1d42bdc65f86253e018895
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2023-08-21 19:11:39 +0200

    change export in routes but error

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,8 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
-const { validateBody , isValidId} = require("../../middlewares");
+const {validateBody} = require("../../middlewares/validateBody");
+const {isValidId} = require("../../middlewares");
 
 const {schemas} = require("../../models/contact");
 
