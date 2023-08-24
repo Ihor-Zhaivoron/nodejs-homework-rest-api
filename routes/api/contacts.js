b098a6112160d31bcbd32882d2439d4f080d9202
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2023-08-24 13:21:49 +0200

    check error path

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,8 +2,7 @@ const express = require("express");
 
 const ctrl = require("../../controllers/contacts");
 
-const {validateBody} = require("../../middlewares/validateBody");
-const {isValidId} = require("../../middlewares");
+const {isValidId, validateBody} = require("../../middlewares");
 
 const {schemas} = require("../../models/contact");
 
