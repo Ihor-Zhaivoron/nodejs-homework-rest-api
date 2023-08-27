838007dd593a1f6ef116049d969b6ef891ab85fc
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2023-08-27 20:52:08 +0200

    create signUp in controllers

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { handleMongooseError } = require("../middlewares");
+
+// const emailRegexp=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      //   match:emailRegexp,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const authSchema = Joi.object({
+  email: Joi.string().required(),
+  password: Joi.string().required(),
+});
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  authSchema,
+};
