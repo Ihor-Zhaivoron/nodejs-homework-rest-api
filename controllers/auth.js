e7305525f5c8d2a76a5126dff756b832182f6137
Ihor-Zhaivoron <105306196+Ihor-Zhaivoron@users.noreply.github.com>
2023-08-28 17:30:16 +0200

    finished login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,10 +1,14 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
 const { User } = require("../models/user");
 const { HttpError, ctrlWrapper } = require("../helpers");
-const bcrypt = require("bcrypt");
+
+
 require("dotenv").config();
 
-// --------------- signUp ----------------------------------------//
-const signUp = async (req, res) => {
+// --------------- signup ----------------------------------------//
+const signup = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
 
@@ -14,9 +18,33 @@ const signUp = async (req, res) => {
   const hashPassword = await bcrypt.hash(password, 10);
   const newUser = await User.create({ ...req.body, password: hashPassword });
 
-  res.status(201).json({ email: newUser.email, password:newUser.password });
+  res.status(201).json({ email: newUser.email, password: newUser.password });
 };
 // ------------------------login---------------------------------
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password is wrong");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(409, "Email or password is wrong");
+  }
+  const payload = {
+    id: user._id,
+  };
+
+  const { SECRET_KEY } = process.env;
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
+  res.status(200).json({
+    token: token,
+    user: { email: user.email, subscription: user.subscription },
+  });
+};
+
 module.exports = {
-    signup: ctrlWrapper(signUp),
-  }; 
+  signup: ctrlWrapper(signup),
+  login: ctrlWrapper(login),
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,14 @@
+// ----------------- router auth/user-------------------------------
 const express = require("express");
+const ctrl = require("../../controllers/auth");
+const { validateBody, authenticate } = require("../../middlewares");
+const { authSchema } = require("../../models/user");
 
-const {validateBody}=require("../../middlewares");
+const router = express.Router();
 
-const {schemas}=require("../../models/user");
+router.get("/signup", validateBody(authSchema), ctrl.signup);
+router.post("/login", validateBody(authSchema), ctrl.console.login);
+router.post("/loguot", authenticate, ctrl.logout);
+router.post("/current", authenticate, ctrl.current);
 
-const router =- express.Router();
-
-// signup
-router.post("/register",validateBody(schemas.registerSchema),)
-
-module.exports=router;
+module.exports = router;
